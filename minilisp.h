b5a786b753a8bf99aac2205774680155a30f788d
Ken Ohata <ohataken@outlook.com>
2016-10-10 20:44:20 +0900

    move

--- minilisp.h
+++ minilisp.h
@@ -12,4 +12,73 @@
 #include <string.h>
 #include <sys/mman.h>
 
+//======================================================================
+// Lisp objects
+//======================================================================
+
+// The Lisp object type
+enum {
+    // Regular objects visible from the user
+    TINT = 1,
+    TCELL,
+    TSYMBOL,
+    TPRIMITIVE,
+    TFUNCTION,
+    TMACRO,
+    TENV,
+    // The marker that indicates the object has been moved to other location by GC. The new location
+    // can be found at the forwarding pointer. Only the functions to do garbage collection set and
+    // handle the object of this type. Other functions will never see the object of this type.
+    TMOVED,
+    // Const objects. They are statically allocated and will never be managed by GC.
+    TTRUE,
+    TNIL,
+    TDOT,
+    TCPAREN,
+};
+
+// Typedef for the primitive function
+struct Obj;
+typedef struct Obj *Primitive(void *root, struct Obj **env, struct Obj **args);
+
+// The object type
+typedef struct Obj {
+    // The first word of the object represents the type of the object. Any code that handles object
+    // needs to check its type first, then access the following union members.
+    int type;
+
+    // The total size of the object, including "type" field, this field, the contents, and the
+    // padding at the end of the object.
+    int size;
+
+    // Object values.
+    union {
+        // Int
+        int value;
+        // Cell
+        struct {
+            struct Obj *car;
+            struct Obj *cdr;
+        };
+        // Symbol
+        char name[1];
+        // Primitive
+        Primitive *fn;
+        // Function or Macro
+        struct {
+            struct Obj *params;
+            struct Obj *body;
+            struct Obj *env;
+        };
+        // Environment frame. This is a linked list of association lists
+        // containing the mapping from symbols to their value.
+        struct {
+            struct Obj *vars;
+            struct Obj *up;
+        };
+        // Forwarding pointer
+        void *moved;
+    };
+} Obj;
+
 #endif
